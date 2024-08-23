11f7df31312ede96f1698f020b178b2c2ea94f72
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-08-23 23:14:27 +0300

    resizing window and full screen opt in-out

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 // TODO cpntinue lesson 6 ,stopped at 20:00
 import * as THREE from "three";
 import gsap from "gsap";
-import { OrbitControls } from "three-orbitcontrols-ts";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 const canvas = document.querySelector("canvas#webgl") as HTMLCanvasElement;
 const size = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 /**
  * cursor
@@ -22,27 +22,14 @@ if (!canvas) alert("canvas is not defined");
 
 const scene = new THREE.Scene();
 
-const aspect = size.width / size.width;
+const getAspect = () => size.width / size.height;
 const angel = 75; // field of view preferred between 45-75
 const near = 0.1;
 const far = 100;
-const camera = new THREE.PerspectiveCamera(angel, aspect, near, far);
-// to remove the distortion caused by the canvas size
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspect,
-//   1 * aspect,
-//   1,
-//   -1,
-//   near,
-//   far
-// );
-const i = 2;
+const camera = new THREE.PerspectiveCamera(angel, getAspect(), near, far);
 
-// camera.position.set(i, i, i);
 camera.position.z = 2;
 scene.add(camera);
-// const axesHelper = new THREE.AxesHelper(5);
-// scene.add(axesHelper);
 
 const cube1 = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
@@ -56,31 +43,39 @@ const renderer = new THREE.WebGLRenderer({
   canvas,
 });
 renderer.setSize(size.width, size.height);
-renderer.render(scene, camera);
-const clock = new THREE.Clock();
 
+renderer.render(scene, camera);
+
+window.addEventListener("resize", (e) => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  camera.aspect = getAspect();
+  camera.updateProjectionMatrix();
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setSize(size.width, size.height);
+});
+// go full screen
+window.addEventListener("dblclick", () => {
+  const fullscreenElement: any = //@ts-expect-error
+    document.fullscreenElement || document.webkitFullscreenElement;
+  // we add webkit prefix to handle safari incompatibility
+  if (!fullscreenElement) {
+    if (document.exitFullscreen) document.exitFullscreen();
+    //@ts-expect-error
+    else if (canvas.webkitRequestFullscreen) canvas.webkitRequestFullscreen();
+  } else {
+    if (document.exitFullscreen) document.exitFullscreen();
+    //@ts-expect-error
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+  }
+});
 const controls = new OrbitControls(camera, canvas);
-// controls.target.y = 2;
 controls.enableDamping = true;
 controls.update();
 
 const animate = () => {
-  const elapsedTime = clock.getElapsedTime();
-  // console.log(camera.position);
-  // camera.position.x += 0.1;
-  // cube1.rotation.y = elapsedTime;
-  // camera.lookAt(cube1.position);
-  // cube1.rotation.y = elapsedTime;
-  // camera.position.x = Math.sin(cursor.x * 2 * Math.PI) * 3;
-  // camera.position.z = Math.cos(cursor.x * 2 * Math.PI) * 3;
-  // camera.position.y = cursor.y;
-  // camera.lookAt(cube1.position);
   controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 };
-// renderer.setAnimationLoop(animate);
 animate();
-// gsap.to(cube1.position, {
-//   duration: 1,
-// });
